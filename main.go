6a14be3026054b4f0e318fe8aa83d301c8e7f708
mortezahajilouei <mortezahajilouei@gmail.com>
2023-03-25 12:14:36 +0330

    add pro

--- main.go
+++ main.go
@@ -1,39 +1,31 @@
 package main
 
 import (
-	"net/http"
+	docs "github.com/MortezaHajilouei/golang-web-server/docs"
+	p1 "github.com/MortezaHajilouei/golang-web-server/pkg1"
 
 	"github.com/gin-gonic/gin"
 	swaggerfiles "github.com/swaggo/files"
 	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
-// @BasePath /api/v1
-
-// PingExample godoc
-// @Summary ping example
-// @Schemes
-// @Description do ping
-// @Tags example
-// @Accept json
-// @Produce json
-// @Success 200 {string} Helloworld
-// @Router /example/helloworld [get]
-func Helloworld(g *gin.Context) {
-	g.JSON(http.StatusOK, "helloworld")
-}
-
 func main() {
 	r := gin.Default()
+	docs.SwaggerInfo.Title = "GardeshPay"
+	docs.SwaggerInfo.Description = "GardeshPay"
+	docs.SwaggerInfo.Host = "localhost:8080"
 	docs.SwaggerInfo.BasePath = "/api/v1"
 	v1 := r.Group("/api/v1")
 	{
-		eg := v1.Group("/example")
+		pk1 := v1.Group("/p1")
 		{
-			eg.GET("/helloworld", Helloworld)
+			pk1.GET("/hello", p1.Helloworld)
+			pk1.GET("/ping", p1.PingExample)
 		}
 	}
-	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerfiles.Handler))
+
+	url := ginSwagger.URL("http://localhost:8080/swagger/doc.json")
+	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerfiles.Handler, url, ginSwagger.DefaultModelsExpandDepth(-1)))
 	r.Run(":8080")
 
 }
